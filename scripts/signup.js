cd9765ce8281592c5053cdc4606927f17cdd188f
Zvono Bednarcik <zvonimir.bednarcik@gmail.com>
2023-01-25 13:21:13 +0100

    fixed some html stuff

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,15 @@
-function forValidate() {
-    const 
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function formValidate() {
+    var goodSubmit = true;
+
+    if(username.value.length < 5) {
+        username.classList.add("input-invalid");
+        goodSubmit = false;
+    }   
+
+    return goodSubmit;
+    
+    
 }
