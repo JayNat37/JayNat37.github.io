fc9569dbb06454d6bfb2bba5f91d811091267bed
Zvono Bednarcik <zvonimir.bednarcik@gmail.com>
2023-01-25 18:59:46 +0100

    more validation

--- scripts/signup.js
+++ scripts/signup.js
@@ -4,12 +4,19 @@ const password = document.getElementById("password");
 const passwordError = document.getElementById("password-error");
 const email = document.getElementById("email");
 const emailError = document.getElementById("email-error");
-
+const fname = document.getElementById("fname");
+const fnameError = document.getElementById("fname-error");
+const sname = document.getElementById("sname");
+const snameError = document.getElementById("sname-error");
+const postal = document.getElementById("postal");
+const postalError = document.getElementById("postal-error");
+const houseno = document.getElementById("houseno");
+const housenoError = document.getElementById("houseno-error");
+const suffix = document.getElementById("suffix");
+const suffixError = document.getElementById("suffix-error");
 
 function formValidate() {
-    if(!username.checkValidity()) {
-        alert("COOCK");
-    }
+    return true;
 }
 /*
     Given an input, error and function that evaluates string from input ("" is good output)
@@ -50,8 +57,8 @@ addValidator(password, passwordError, (string) => {
 })
 addValidator(email, emailError, (string) => {
     var atIndex = string.indexOf('@');
-    var dotIndex = string.indexOf('.');
-    
+    var dotIndex = string.lastIndexOf('.');
+
     if(atIndex == -1 || dotIndex == -1 
             || atIndex > dotIndex || atIndex == 0 
             || dotIndex == string.length -1) {
@@ -59,4 +66,49 @@ addValidator(email, emailError, (string) => {
         "Email formated incorrectly"];
     }
     return ["", ""];
+});
+addValidator(fname, fnameError, (string) => {
+    if(string.length < 2) {
+        return ["Name needs to be at least 2 characters long.", 
+        "Name too short."];
+    } else {
+        return ["",""];
+    }
+})
+
+addValidator(sname, snameError, (string) => {
+    if(string.length < 2) {
+        return ["Name needs to be at least 2 characters long.", 
+        "Name too short."];
+    } else {
+        return ["",""];
+    }
+})
+// no regex means terrible spaghetti code
+addValidator(postal, postalError, (string) => {
+    if(string.length != 6 || 
+        isNaN(string.slice(0,4)) || 
+        string.charAt(4).toUpperCase() == string.charAt(4).toLowerCase() ||
+        string.charAt(5).toUpperCase() == string.charAt(5).toLowerCase()    ) {
+        return ["Correct format: 2628BA"];
+    } else {
+        return ["",""];
+    }
+})
+
+addValidator(houseno, housenoError, (string) => {
+    if(isNaN(string)) {
+        return ["Must be a number"];
+    } else {
+        return ["",""];
+    }
+})
+addValidator(suffix, suffixError, (string) => {
+    if(string.length > 1 || 
+        (string.length == 1 &&
+            string.charAt(0).toUpperCase() == string.charAt(0).toLowerCase())) {
+        return ["Must be a letter"];
+    } else {
+        return ["",""];
+    }
 });
